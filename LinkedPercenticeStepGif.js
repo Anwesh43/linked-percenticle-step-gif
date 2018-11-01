f04631fd64b0195af21d40d4eb7be48a3c7262f6
Anwesh43 <anweshthecool0@gmail.com>
2018-11-01 18:38:37 +0530

    adding PSNode class

--- LinkedPercenticeStepGif.js
+++ LinkedPercenticeStepGif.js
@@ -1,3 +1,5 @@
+const w = 500, h = 500
+const nodes = 5
 class State {
     constructor() {
         this.scale = 0
@@ -15,10 +17,65 @@ class State {
         }
     }
 
-    startUpdating(cb) {
+    startUpdating() {
         if (this.dir == 0) {
             this.dir = 1 - 2 * this.prevScale
-            cb()
         }
+    }
+}
+
+class PSNode {
+    constructor(i) {
+        this.i = i
+        this.state = new State()
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new PSNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context) {
+        const gap = w / (nodes + 1)
+        const size = gap / 3
+        const r = size / 3
+        context.save()
+        context.translate(gap * this.i + gap, h/2)
+        context.save()
+        context.rotate(Math.PI/12)
+        context.beginPath()
+        context.moveTo(0, -size)
+        context.lineTo(0, size)
+        context.stroke()
+        context.restore()
+        for (var j = 0; j < 2; j++) {
+            const sc = Math.min(0.5, Math.max(this.state.scale - 0.5 * j, 0)) * 2
+            context.save()
+            context.rotate(Math.PI * j)
+            context.translate(size/4, size/4)
+            for(var k = 0; k <= 360 * sc; k++) {
+                const x = r * Math.cos(k * Math.PI/180), y = r * Math.sin(k * Math.PI/180)
+                context.beginPath()
+                if (k == 0) {
+                    context.moveTo(x, y)
+                } else {
+                    context.lineTo(x, y)
+                }
+            }
+            context.stroke()
+            context.restore()
+        }
+        context.restore()
+    }
+
+    update(cb) {
+        this.state.update(cb)
+    }
+
+    startUpdating() {
+        this.state.startUpdating()
     }
 }
